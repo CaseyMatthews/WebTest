a105df00ae2df4930ab3c99d69dbc2cd8d0ffe45
CaseyMatthews <casey-matthews@live.com>
2019-11-23 19:26:30 -0700

    Added header navigation menu and sub-menu elements (untested).

--- headernewnew.py
+++ headernewnew.py
@@ -53,11 +53,151 @@ class Header(Page):
 
         self.irc_link = ElementMetaData(class_name='icon-freenode')
 
+        ##########################
+        # Navigation bar elements.
+        ##########################
+
+        self.nav_about = ElementMetaData(id='about')
+
+        self.nav_downloads = ElementMetaData(id='downloads')
+
+        self.nav_documentation = ElementMetaData(id='documentation')
+
+        self.nav_community = ElementMetaData(id='community')
+
+        self.nav_success_stories = ElementMetaData(id='success-stories')
+
+        self.nav_news = ElementMetaData(id='news')
+
+        self.nav_events = ElementMetaData(id='events')
+
+        ##############################
+        # Navigation bar sub-elements.
+        ##############################
+
+        # About sub-menu elements.
+
+        self.nav_about_apps = ElementMetaData(xpath='//*[@id="about"]/ul/li[1]/a')
+
+        self.nav_about_quotes = ElementMetaData(xpath='//*[@id="about"]/ul/li[2]/a')
+
+        self.nav_about_getting_started = ElementMetaData(xpath='//*[@id="about"]/ul/li[3]/a')
+
+        self.nav_about_help = ElementMetaData(xpath='//*[@id="about"]/ul/li[4]/a')
+
+        self.nav_about_py_brochure = ElementMetaData(xpath='//*[@id="about"]/ul/li[5]/a')
+
+        self.nav_about_learn_more = ElementMetaData(xpath='//*[@id="about"]/ul/li[6]/p/a')
+
+        # Downloads sub-menu elements.
+
+        self.nav_downloads_all_releases = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[1]/a')
+
+        self.nav_downloads_source_code = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[2]/a')
+
+        self.nav_downloads_windows = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[3]/a')
+
+        self.nav_downloads_maco = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[4]/a')
+
+        self.nav_downloads_other_platforms = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[5]/a')
+
+        self.nav_downloads_license = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[6]/a')
+
+        self.nav_downloads_alt_imps = ElementMetaData(xpath='//*[@id="downloads"]/ul/li[7]/a')
+
+        # Documentation sub-menu elements.
+
+        self.nav_documentation_docs = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[1]/a')
+
+        self.nav_documentation_audio_visual = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[2]/a')
+
+        self.nav_documentation_beginners_guide = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[3]/a')
+
+        self.nav_documentation_devs_guide = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[4]/a')
+
+        self.nav_documentation_faq = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[5]/a')
+
+        self.nav_documentation_non_english_docs = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[6]/a')
+
+        self.nav_documentation_pep_index = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[7]/a')
+
+        self.nav_documentation_py_books = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[8]/a')
+
+        self.nav_documentation_py_essays = ElementMetaData(xpath='//*[@id="documentation"]/ul/li[9]/a')
+
+        # Community sub-menu elements.
+
+        self.nav_comm_survey = ElementMetaData(xpath='//*[@id="community"]/ul/li[1]/a')
+
+        self.nav_comm_diversity = ElementMetaData(xpath='//*[@id="community"]/ul/li[2]/a')
+
+        self.nav_comm_mailing_lists = ElementMetaData(xpath='//*[@id="community"]/ul/li[3]/a')
+
+        self.nav_comm_irc = ElementMetaData(xpath='//*[@id="community"]/ul/li[4]/a')
+
+        self.nav_comm_forums = ElementMetaData(xpath='//*[@id="community"]/ul/li[5]/a')
+
+        self.nav_comm_annual_impact_report = ElementMetaData(xpath='//*[@id="community"]/ul/li[6]/a')
+
+        self.nav_comm_py_conferences = ElementMetaData(xpath='//*[@id="community"]/ul/li[7]/a')
+
+        self.nav_comm_spec_interest_groups = ElementMetaData(xpath='//*[@id="community"]/ul/li[8]/a')
+
+        self.nav_comm_py_logo = ElementMetaData(xpath='//*[@id="community"]/ul/li[9]/a')
+
+        self.nav_comm_py_wiki = ElementMetaData(xpath='//*[@id="community"]/ul/li[10]/a')
+
+        self.nav_comm_merchandise = ElementMetaData(xpath='//*[@id="community"]/ul/li[11]/a')
+
+        self.nav_comm_awards = ElementMetaData(xpath='//*[@id="community"]/ul/li[12]/a')
+
+        self.nav_comm_code_of_conduct = ElementMetaData(xpath='//*[@id="community"]/ul/li[13]/a')
+
+        # Success stories sub-menu elements.
+
+        self.nav_success_stories_arts = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[1]/a')
+
+        self.nav_success_stories_business = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[2]/a')
+
+        self.nav_success_stories_education = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[3]/a')
+
+        self.nav_success_stories_engineering = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[4]/a')
+
+        self.nav_success_stories_government = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[5]/a')
+
+        self.nav_success_stories_scientific = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[6]/a')
+
+        self.nav_success_stories_sw_dev = ElementMetaData(xpath='//*[@id="success-stories"]/ul/li[7]/a')
+
+        # News sub-menu elements.
+
+        self.nav_news_py_news = ElementMetaData(xpath='//*[@id="news"]/ul/li[1]/a')
+
+        self.nav_news_psf_newsletter = ElementMetaData(xpath='//*[@id="news"]/ul/li[2]/a')
+
+        self.nav_news_community_news = ElementMetaData(xpath='//*[@id="news"]/ul/li[3]/a')
+
+        self.nav_news_psf_news = ElementMetaData(xpath='//*[@id="news"]/ul/li[4]/a')
+
+        self.nav_news_pycon_news = ElementMetaData(xpath='//*[@id="news"]/ul/li[5]/a')
+
+        # Events sub-menu elements.
+
+        self.nav_events_py_events = ElementMetaData(xpath='//*[@id="events"]/ul/li[1]/a')
+
+        self.nav_events_user_group_events = ElementMetaData(xpath='//*[@id="events"]/ul/li[2]/a')
+
+        self.nav_events_py_events_archive = ElementMetaData(xpath='//*[@id="events"]/ul/li[3]/a')
+
+        self.nav_events_user_group_events_archive = ElementMetaData(xpath='//*[@id="events"]/ul/li[4]/a')
+
+        self.nav_events_submit_event = ElementMetaData(xpath='//*[@id="events"]/ul/li[5]/a')
+
 
 if __name__ == "__main__":
 
-    hdr = Header('http://www.python.org', webdriver.Chrome)
+    hdr = Header("http://www.python.org", webdriver.Chrome)
 
-    hdr.hover_over(hdr.social_drop_down)
+    hdr.hover_over(hdr.nav_about)
 
-    hdr.click(hdr.twitter_link)
+    hdr.click(hdr.nav_about_apps)
