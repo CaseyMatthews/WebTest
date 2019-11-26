ab5a4ae845b0cfda8304d168283e4769f5c10420
CaseyMatthews <casey-matthews@live.com>
2019-11-26 12:46:22 -0700

    Tests for community navigation dropdown.

--- tests/test_header.py
+++ tests/test_header.py
@@ -59,7 +59,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertIn('Python', self.driver.title)
 
-    def test_header_search_field_enter(self):
+    def test_header_search_field(self):
 
         self.header.search_field.send_keys('pycon' + Keys.ENTER)
 
@@ -75,27 +75,21 @@ class MyTestCase(unittest.TestCase):
 
     def test_social_media_link_facebook(self):
 
-        self.driver.hover_over(self.header.social_drop_down)
-
-        self.header.facebook_link.click()
-
-        self.assertIn('facebook', self.driver.page_source)
+        self.dropdown_link(self.header.social_drop_down,
+                           self.header.facebook_link,
+                           'https://www.facebook.com/pythonlang?fref=ts')
 
     def test_social_media_link_twitter(self):
 
-        self.driver.hover_over(self.header.social_drop_down)
-
-        self.header.twitter_link.click()
-
-        self.assertIn('twitter', self.driver.page_source)
+        self.dropdown_link(self.header.social_drop_down,
+                           self.header.twitter_link,
+                           'https://twitter.com/ThePSF')
 
     def test_social_media_link_irc(self):
 
-        self.driver.hover_over(self.header.social_drop_down)
-
-        self.header.irc_link.click()
-
-        self.assertIn('Internet Relay Chat', self.driver.page_source)
+        self.dropdown_link(self.header.social_drop_down,
+                           self.header.irc_link,
+                           'https://www.python.org/community/irc/')
 
     def test_social_links_no_hover_not_clickable(self):
 
@@ -178,131 +172,41 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual('https://www.python.org/', self.driver.current_url)
 
-    def test_about_applications_link_not_displayed(self):
+    def test_about_apps(self):
 
-        self.assertFalse(self.header.nav_about_apps.is_displayed())
+        self.dropdown_link(self.header.nav_about,
+                           self.header.nav_about_apps,
+                           'https://www.python.org/about/apps/')
 
-    def test_about_quotes_link_not_displayed(self):
+    def test_about_quotes(self):
 
-        self.assertFalse(self.header.nav_about_quotes.is_displayed())
+        self.dropdown_link(self.header.nav_about,
+                           self.header.nav_about_quotes,
+                           'https://www.python.org/about/quotes/')
 
-    def test_about_getting_started_link_not_displayed(self):
+    def test_about_getting_started(self):
 
-        self.assertFalse(
-            self.header.nav_about_getting_started.is_displayed())
+        self.dropdown_link(self.header.nav_about,
+                           self.header.nav_about_getting_started,
+                           'https://www.python.org/about/gettingstarted/')
 
-    def test_about_help_link_not_displayed(self):
+    def test_about_help(self):
 
-        self.assertFalse(self.header.nav_about_help.is_displayed())
+        self.dropdown_link(self.header.nav_about,
+                           self.header.nav_about_help,
+                           'https://www.python.org/about/help/')
 
-    def test_about_python_brochure_link_not_displayed(self):
+    def test_about_python_brochure(self):
 
-        self.assertFalse(self.header.nav_about_py_brochure.is_displayed())
+        self.dropdown_link(self.header.nav_about,
+                           self.header.nav_about_py_brochure,
+                           'https://brochure.getpython.info/')
 
-    def test_about_learn_more_link_not_displayed(self):
+    def test_about_learn_more(self):
 
-        self.assertFalse(self.header.nav_about_learn_more.is_displayed())
-
-    def test_about_applications_display_on_hover(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.assertTrue(self.header.nav_about_apps.is_displayed())
-
-    def test_about_quotes_display_on_hover(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.assertTrue(self.header.nav_about_quotes.is_displayed())
-
-    def test_about_getting_started_display_on_hover(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.assertTrue(
-            self.header.nav_about_getting_started.is_displayed())
-
-    def test_about_help_display_on_hover(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.assertTrue(self.header.nav_about_help.is_displayed())
-
-    def test_about_python_brochure_display_on_hover(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.assertTrue(self.header.nav_about_py_brochure.is_displayed())
-
-    def test_about_learn_more_display_on_hover(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.assertTrue(self.header.nav_about_learn_more.is_displayed())
-
-    def test_about_apps_click(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.header.nav_about_apps.click()
-
-        self.assertEqual('https://www.python.org/about/apps/', self.driver.current_url)
-
-    def test_about_quotes_click(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.header.nav_about_quotes.click()
-
-        self.assertEqual('https://www.python.org/about/quotes/', self.driver.current_url)
-
-    def test_about_getting_started_click(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.header.nav_about_getting_started.click()
-
-        self.assertEqual('https://www.python.org/about/gettingstarted/', self.driver.current_url)
-
-    def test_about_help_click(self):
-
-        self.driver.hover_over(self.header.nav_about)
-
-        self.header.nav_about_py_brochure.click()
-
-        self.assertEqual('https://brochure.getpython.info/', self.driver.current_url)
-
-    def test_downloads_all_releases_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_all_releases.is_displayed())
-
-    def test_downloads_source_code_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_source_code.is_displayed())
-
-    def test_downloads_windows_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_dwnld_windows.is_displayed())
-
-    def test_downloads_mac_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_mac.is_displayed())
-
-    def test_downloads_other_platforms_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_other_platforms.is_displayed())
-
-    def test_downloads_license_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_license.is_displayed())
-
-    def test_downloads_alternative_implementation_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_alt_imps.is_displayed())
-
-    def test_downloads_download_windows_not_displayed(self):
-
-        self.assertFalse(self.header.nav_downloads_dwnld_windows.is_displayed())
+        self.dropdown_link(self.header.nav_about,
+                           self.header.nav_about_learn_more,
+                           'https://www.python.org/about/')
 
     def test_downloads_all_releases(self):
 
@@ -346,9 +250,149 @@ class MyTestCase(unittest.TestCase):
                            self.header.nav_downloads_alt_imps,
                            'https://www.python.org/download/alternatives/')
 
-    def test_documentation_docs_not_displayed(self):
+    def test_documentation_docs(self):
 
-        self.assertFalse(self.header.nav_documentation_docs.is_displayed())
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_docs,
+                           'https://www.python.org/doc/')
+
+    def test_documentation_audio_visual_talks(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_audio_visual,
+                           'https://www.python.org/doc/av/')
+
+    def test_documentation_beginners_guide(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_beginners_guide,
+                           'https://wiki.python.org/moin/BeginnersGuide')
+
+    def test_documentation_devs_guide(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_devs_guide,
+                           'https://devguide.python.org/')
+
+    def test_documentation_faqs(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_faq,
+                           'https://docs.python.org/3/faq/')
+
+    def test_documentation_non_english_docs(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_non_english_docs,
+                           'https://wiki.python.org/moin/Languages')
+
+    def test_documentation_pep_index(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_pep_index,
+                           'https://www.python.org/dev/peps/')
+
+    def test_documentation_python_books(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_py_books,
+                           'https://wiki.python.org/moin/PythonBooks')
+
+    def test_documentation_python_essays(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_py_essays,
+                           'https://www.python.org/doc/essays/')
+
+    def test_documentation_python_3x_docs(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_py3_docs,
+                           'https://docs.python.org/3/')
+
+    def test_documentation_python_2x_docs(self):
+
+        self.dropdown_link(self.header.nav_documentation,
+                           self.header.nav_documentation_py2_docs,
+                           'https://docs.python.org/2/')
+
+    def test_community_community_survey(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_survey,
+                           'https://www.python.org/community/survey/')
+
+    def test_community_diversity(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_diversity,
+                           'https://www.python.org/community/diversity/')
+
+    def test_community_mailing_lists(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_mailing_lists,
+                           'https://www.python.org/community/lists/')
+
+    def test_community_irc(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_irc,
+                           'https://www.python.org/community/irc/')
+
+    def test_community_forums(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_forums,
+                           'https://www.python.org/community/forums/')
+
+    def test_community_pas_annual_impact_report(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_annual_impact_report,
+                           'https://www.python.org/psf/annual-report/2019/')
+
+    def test_community_python_conferences(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_py_conferences,
+                           'https://www.python.org/community/workshops/')
+
+    def test_community_special_interest_groups(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_spec_interest_groups,
+                           'https://www.python.org/community/sigs/')
+
+    def test_community_python_logo(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_py_logo,
+                           'https://www.python.org/community/logos/')
+
+    def test_community_python_wiki(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_py_wiki,
+                           'https://wiki.python.org/moin/')
+
+    def test_community_merchandise(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_merchandise,
+                           'https://www.python.org/community/merchandise/')
+
+    def test_community_community_awards(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_awards,
+                           'https://www.python.org/community/awards/')
+
+    def test_community_code_of_conduct(self):
+
+        self.dropdown_link(self.header.nav_community,
+                           self.header.nav_comm_code_of_conduct,
+                           'https://www.python.org/psf/conduct/')
 
 
 if __name__ == '__main__':
